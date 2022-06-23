219aa21e09e5b062eecc19df9aaa58cb86a31c09
Jailson Souza Carvalho <jailsonsouzaa714@gmail.com>
2022-06-23 20:26:51 -0300

    Upando os Códigos

--- Todas as Questões/URI 1179.pas
+++ Todas as Questões/URI 1179.pas
@@ -1,13 +1,43 @@
-#include <stdio.h>
- 
-int main() {
- 
-    int i;
-    double n;
-    scanf("%lf",&n);
-    for(i = 0; i < 100; i++){
-        printf("N[%d] = %.4lf\n",i,n);
-        n /= 2.0;
-    }
-    return 0;
-}
+program uri1179;
+var
+x, i, j, qp, qi : integer;
+par : array[1..5] of integer;
+impar : array[1..5] of integer;
+
+begin
+  qp := 0;
+  qi := 0;
+
+  for i:=0 to 14 do
+    begin
+        ReadLn(x);
+
+        if x % 2 = 0 then
+            begin
+                par[qp] := x;
+                qp += 1;
+                if qp = 5 then
+                    begin
+                        for j:=0 to 4 do
+                            WriteLn('par[',j,'] = ',par[j]);
+                        qp := 0;
+                    end;
+            end;
+        else
+            begin
+                impar[qi] := x;
+                qi += 1;
+                if qi = 5 then
+                    begin
+                        for j:=0 to 4 do
+                            WriteLn('impar[',j,'] = ',impar[j]);
+                        qi := 0;
+                    end;
+            end;
+    end;
+
+  for i:=0 to qi do
+    WriteLn('impar[',j,'] = ',impar[j]);
+  for i:=0 to qp do
+    WriteLn('par[',j,'] = ',par[j]);
+end.
--- Todas as Questões/URI 1180.pas
+++ Todas as Questões/URI 1180.pas
@@ -1,4 +1,35 @@
-program uri1000;
-begin
-  WriteLn('Hello World!');
-end.
+#include <stdio.h>
+ 
+int main() {
+    int par[5], impar[5];
+    int qp = 0, qi = 0;
+    int x;
+    int i,j;
+    
+    for(i = 0; i < 15; i++){
+        scanf("%d",&x);
+        
+        if(x%2 == 0){
+            par[qp] = x;
+            qp++;
+            if(qp == 5){
+                for(j = 0;j < 5; j++)
+                    printf("par[%d] = %d\n",j,par[j]);
+                    qp = 0;
+              }
+            }else{
+                impar[qi] = x;
+                qi++;
+                if(qi == 5){
+                for(j = 0;j < 5; j++)
+                    printf("impar[%d] = %d\n",j,impar[j]);
+                    qi = 0;
+              }
+            }
+        }
+ 
+    for(j = 0;j < qi; j++)
+         printf("impar[%d] = %d\n",j,impar[j]);
+    for(j = 0;j < qp; j++)
+         printf("par[%d] = %d\n",j,par[j]);
+}
